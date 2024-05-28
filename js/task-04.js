bf2a5b298ad086a4a81f586c860b98d16fa6d376
Frapsy32 <frapsenko@ukr.net>
2024-05-28 17:07:01 -0300

    hw_update_03

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,23 @@
-const inputEmail = document.querySelector("#email");
-const inputPassword = document.querySelector("#password");
-const inputForm = document.querySelector(".login-form");
+const formElement = document.querySelector(".login-form");
 
-inputForm.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
+formElement.addEventListener("submit", (event) => {
   event.preventDefault();
-}
+
+  const emailInput = formElement.elements["email"];
+  const passwordInput = formElement.elements["password"];
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    return alert("All form fields must be filled in");
+  }
+
+  const formNew = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formNew);
+  form.reset();
+});
